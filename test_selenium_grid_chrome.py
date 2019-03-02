c0e003d7dd87bc2ac95ff95a1ed09245869cb8af
thiswind <thiswind@gmail.com>
2019-03-03 01:16:57 +0800

    init

---
+++ test_selenium_grid_chrome.py
@@ -0,0 +1,22 @@
+'''Test for launching qxf2.com in AWS machine through grid'''
+ 
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+ 
+def test_chrome():
+    #Set desired capabilities
+    desired_capabilities = DesiredCapabilities.CHROME
+    desired_capabilities['platform'] = 'LINUX'
+ 
+    #Using Remote connection to connect to aws grid - Used manager ip
+    url = "http://113.55.14.53:4444/wd/hub"
+    driver  = webdriver.Remote(url, desired_capabilities)
+ 
+    #Checking the driver session
+    print (driver)
+ 
+    #Launch qxf2 and print title
+    driver.get("http://www.qxf2.com")
+ 
+    print (driver.title)
+    driver.quit()
